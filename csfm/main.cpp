87ba430a55d16aa8a5c7375bad7b003e9fb1c8e3
42yeah <42yeah@gmx.fr>
2021-03-15 08:50:04 +0800

    Initial Commit

---
+++ csfm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  csfm
+//
+//  Created by apple on 15/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
